8d8dbbac8926c6cc2c91d6963ba6f670b8e808b8
Sagnik <palsagnik2102@gmail.com>
2025-08-11 01:51:18 +0530

    CH2: L3
    Signed-off-by: Sagnik <palsagnik2102@gmail.com>

---
+++ cmd/udpsender/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	serverAddr := "localhost:42069"
+
+	udpAddr, err := net.ResolveUDPAddr("udp", serverAddr)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error resolving udp addr: %v\n", err)
+		os.Exit(1)
+	}
+
+	conn, err := net.DialUDP("udp", nil, udpAddr)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error dialing: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	fmt.Printf("Sending to %s. Type your message and press Enter to send. Press Ctrl+C to exit.\n", serverAddr)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		n, err := conn.Write([]byte(line))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("written %d bytes\n", n)
+	}
+}
