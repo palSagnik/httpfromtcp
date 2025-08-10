e68abf90be84cdb348a401077a5344555fb49878
Sagnik <palsagnik2102@gmail.com>
2025-08-10 23:05:09 +0530

    CH1: L5
    Signed-off-by: Sagnik <palsagnik2102@gmail.com>

--- main.go
+++ main.go
@@ -19,29 +19,45 @@ func main() {
 	fmt.Printf("Reading data from %s\n", filePath)
 	fmt.Println("=====================================")
 
-	var line string
-	for {
-		data := make([]byte, 8)
-		n, err := file.Read(data)
-		if err != nil {
-			if errors.Is(err, io.EOF) {
-				break
-			}
-			fmt.Printf("error: %s\n", err.Error())
-			break
-		}
-
-		data = data[:n]
-		if i := bytes.IndexByte(data, '\n'); i != -1 {
-			line += string(data[:i])
-			data = data[i + 1:]
-			fmt.Printf("read: %s\n", line)
-			line = ""
-		}
-		line += string(data)
-	}
-
-	if len(line) > 0 {
+	lines := getLinesChannel(file)
+	for line := range lines {
 		fmt.Printf("read: %s\n", line)
 	}
+}
+
+func getLinesChannel(file io.ReadCloser) <-chan string {
+	out := make(chan string)
+
+	go func ()  {
+		defer file.Close()
+		defer close(out)
+
+		var line string
+		for {
+			data := make([]byte, 8)
+			n, err := file.Read(data)
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
+				break
+			}
+
+			data = data[:n]
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				line += string(data[:i])
+				data = data[i + 1:]
+				out <- line
+				line = ""
+			}
+			line += string(data)
+		}
+
+		if len(line) > 0 {
+			out <- line
+		}
+
+	}()
+	return out
 }
